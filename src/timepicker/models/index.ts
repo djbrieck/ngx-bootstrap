e329b2594c082b7aa3414f50c8edaac2fad62147
Daniel J Brieck Jr <djbrieck@gmail.com>
2023-01-24 22:27:41 -0800

    cleaning up multipe imports from core

--- src/timepicker/models/index.ts
+++ src/timepicker/models/index.ts
@@ -1,6 +1,5 @@
-import { InjectionToken } from '@angular/core';
+import { InjectionToken, Type } from '@angular/core';
 import { ControlValueAccessor } from '@angular/forms';
-import { Type } from '@angular/core';
 
 export interface ControlValueAccessorModel {
   provide: InjectionToken<ControlValueAccessor>;
